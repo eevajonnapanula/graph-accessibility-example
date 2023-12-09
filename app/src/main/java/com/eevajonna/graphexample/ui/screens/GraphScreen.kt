0b049a57c89a1fc73f0224c0f1e4130871f63ae9
Eevis Panula <eevajonna.panula@gmail.com>
2023-12-09 06:48:13 +0200

    Add alternative to touch (#5)

--- app/src/main/java/com/eevajonna/graphexample/ui/screens/GraphScreen.kt
+++ app/src/main/java/com/eevajonna/graphexample/ui/screens/GraphScreen.kt
@@ -10,10 +10,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,7 +38,7 @@ fun GraphScreen(modifier: Modifier = Modifier) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(GraphScreen.innerPadding),
-        modifier = modifier,
+        modifier = modifier.verticalScroll(rememberScrollState()),
     ) {
         val graphColors = GraphColors(
             totalColor = MaterialTheme.colorScheme.primary,

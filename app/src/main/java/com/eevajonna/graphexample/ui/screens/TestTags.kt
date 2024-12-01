73a29580abcff596591655f4660183fe910b07dd
Eeva-Jonna Panula <eevajonna.panula@gmail.com>
2024-12-01 08:14:36 +0200

    Add tests for alternative navigation options

---
+++ app/src/main/java/com/eevajonna/graphexample/ui/screens/TestTags.kt
@@ -0,0 +1,8 @@
+package com.eevajonna.graphexample.ui.screens
+
+object TestTags {
+    const val labelsTestTag = "chart-labels-test-tag"
+    const val chartTestTag = "chart-test-tag"
+    const val rightButtonTestTag = "chart-right-button-test-tag"
+    const val leftButtonTestTag = "chart-left-button-test-tag"
+}
